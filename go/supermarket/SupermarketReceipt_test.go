9cbe180f28f7480a4620e80b414be620d41f9f5d
rodmartinez <izeka1983@hotmail.com>
2021-04-15 18:08:40 -0300

    Add basic unit tests

--- go/supermarket/SupermarketReceipt_test.go
+++ go/supermarket/SupermarketReceipt_test.go
@@ -1,16 +1,22 @@
 package supermarket
 
 import (
+	"testing"
+
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"testing"
 )
 
-type FakeCatalog struct {
-    _products map[string]Product
-    _prices map[string]float64
-}
+var toothbrush = Product{name: "toothbrush", unit: Each}
+var apples = Product{name: "apples", unit: Kilo}
+var rice = Product{name: "rice", unit: Each}
+var toothpaste = Product{name: "toothpaste", unit: Each}
+var cherrytomatoes = Product{name: "cherrytomatoes", unit: Each}
 
+type FakeCatalog struct {
+	_products map[string]Product
+	_prices   map[string]float64
+}
 
 func (c FakeCatalog) unitPrice(product Product) float64 {
 	return c._prices[product.name]
@@ -25,19 +31,59 @@ func NewFakeCatalog() *FakeCatalog {
 	var c FakeCatalog
 	c._products = make(map[string]Product)
 	c._prices = make(map[string]float64)
+
+	c.addProduct(toothbrush, 0.99)
+	c.addProduct(apples, 1.99)
+	c.addProduct(rice, 2.49)
+	c.addProduct(toothpaste, 1.79)
+	c.addProduct(cherrytomatoes, 0.69)
+
 	return &c
 }
 
-func TestTenPercentDiscount(t *testing.T) {
-	// ARRANGE
-	var toothbrush = Product{name: "toothbrush", unit: Each}
-	var apples = Product{name: "apples", unit: Kilo}
+func TestNoDiscounts(t *testing.T) {
+
 	var catalog = NewFakeCatalog()
-	catalog.addProduct(toothbrush, 0.99)
-	catalog.addProduct(apples, 1.99)
 
 	var teller = NewTeller(catalog)
-	teller.addSpecialOffer(TenPercentDiscount, toothbrush, 10.0)
+
+	var cart = NewShoppingCart()
+	cart.addItemQuantity(toothbrush, 1)
+
+	// ACT
+	var receipt = teller.checksOutArticlesFrom(cart)
+
+	// ASSERT
+	assert.Equal(t, 0.99, receipt.totalPrice())
+	assert.Equal(t, 0, len(receipt.discounts))
+	require.Equal(t, 1, len(receipt.items))
+}
+
+func TestTwoForAmount(t *testing.T) {
+
+	var catalog = NewFakeCatalog()
+
+	var teller = NewTeller(catalog)
+	teller.addSpecialOffer(TwoForAmount, toothbrush, catalog._prices["toothbrush"])
+
+	var cart = NewShoppingCart()
+	cart.addItemQuantity(toothbrush, 2)
+
+	// ACT
+	var receipt = teller.checksOutArticlesFrom(cart)
+
+	// ASSERT
+	assert.Equal(t, 0.99, receipt.totalPrice())
+	assert.Equal(t, 1, len(receipt.discounts))
+	require.Equal(t, 1, len(receipt.items))
+}
+
+func TestTwentyPercentDiscount(t *testing.T) {
+
+	var catalog = NewFakeCatalog()
+
+	var teller = NewTeller(catalog)
+	teller.addSpecialOffer(TenPercentDiscount, apples, 20.0)
 
 	var cart = NewShoppingCart()
 	cart.addItemQuantity(apples, 2.5)
@@ -46,11 +92,64 @@ func TestTenPercentDiscount(t *testing.T) {
 	var receipt = teller.checksOutArticlesFrom(cart)
 
 	// ASSERT
-	assert.Equal(t, 4.975, receipt.totalPrice())
-	assert.Equal(t, 0, len(receipt.discounts))
+	assert.Equal(t, 3.98, receipt.totalPrice())
+	assert.Equal(t, 1, len(receipt.discounts))
 	require.Equal(t, 1, len(receipt.items))
-	var receiptItem = receipt.items[0]
-    assert.Equal(t, 1.99, receiptItem.price)
-	assert.Equal(t, 2.5*1.99, receiptItem.totalPrice)
-	assert.Equal(t, 2.5, receiptItem.quantity)
+}
+
+func TestTenPercentDiscount(t *testing.T) {
+
+	var catalog = NewFakeCatalog()
+
+	var teller = NewTeller(catalog)
+
+	var cart = NewShoppingCart()
+	cart.addItemQuantity(rice, 2)
+	teller.addSpecialOffer(TenPercentDiscount, rice, 10.0)
+
+	// ACT
+	var receipt = teller.checksOutArticlesFrom(cart)
+
+	// ASSERT
+	assert.Equal(t, 4.48, receipt.totalPrice())
+	assert.Equal(t, 1, len(receipt.discounts))
+	require.Equal(t, 1, len(receipt.items))
+}
+
+func TestFiveForAmount(t *testing.T) {
+
+	var catalog = NewFakeCatalog()
+
+	var teller = NewTeller(catalog)
+
+	var cart = NewShoppingCart()
+	cart.addItemQuantity(toothpaste, 5)
+	teller.addSpecialOffer(FiveForAmount, toothpaste, 7.49)
+
+	// ACT
+	var receipt = teller.checksOutArticlesFrom(cart)
+
+	// ASSERT
+	assert.Equal(t, 7.49, receipt.totalPrice())
+	assert.Equal(t, 1, len(receipt.discounts))
+	require.Equal(t, 1, len(receipt.items))
+}
+
+func TestThreeForTwo(t *testing.T) {
+
+	var catalog = NewFakeCatalog()
+
+	var teller = NewTeller(catalog)
+
+	var cart = NewShoppingCart()
+	cart.addItemQuantity(cherrytomatoes, 3)
+	teller.addSpecialOffer(ThreeForTwo, cherrytomatoes, 0.69)
+
+	// ACT
+	var receipt = teller.checksOutArticlesFrom(cart)
+
+	// ASSERT
+	assert.Equal(t, 1.38, receipt.totalPrice())
+	assert.Equal(t, 1, len(receipt.discounts))
+	require.Equal(t, 1, len(receipt.items))
 }
